90dc46ad298775b57713ba446d0ee00286c0520c
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-09-25 12:00:10 +0900

    feat: weak05 - if

---
+++ Week05/main3.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	brokenWords := "cs r?cks~"
+	replaceWords := strings.NewReplacer("?", "o")
+	fixedWrods := replaceWords.Replace(brokenWords)
+	fmt.Println(fixedWrods)
+}
