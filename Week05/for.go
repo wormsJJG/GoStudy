b3b12b32a80558d7ed84399719571dd101c71120
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-09-25 12:25:56 +0900

    week5 class finish

---
+++ Week05/for.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().Unix())
+	answer := rand.Intn(100) + 1
+
+	fmt.Println("Guess number (1 ~ 100) : ")
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < 10; i++ {
+
+		fmt.Println("U chace :", 10-i)
+		fmt.Println("Guess number : ")
+		inputNumberString, err := reader.ReadString('\n')
+
+		if err != nil {
+
+			log.Fatal(err)
+		}
+
+		inputNumberString = strings.TrimSpace(inputNumberString)
+		inputNumber, err := strconv.Atoi(inputNumberString)
+
+		if err != nil {
+
+			log.Fatal(err)
+		}
+
+		if inputNumber < answer {
+
+			fmt.Println("Ur guess number is lower than answer")
+		} else if inputNumber > answer {
+
+			fmt.Println("Ur guess number is heigher than answer")
+		} else {
+
+			fmt.Println("Gread U got the number Congratulations")
+			break
+		}
+	}
+}
--- Week05/main.go
+++ Week05/main.go
@@ -1,36 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
 
-	fmt.Println("Input score : ")
-	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n')
-	if err != nil {
-
-		log.Fatal(err)
-	}
-
-	inputScore = strings.TrimSpace(inputScore)
-	score, err := strconv.ParseFloat(inputScore, 64)
-	var grade string
-
-	if score >= 90 {
-
-		grade = "A grade"
-	} else {
-
-		grade = "under A gorade"
-	}
-
-	fmt.Println(grade)
-	fmt.Println("You will get", grade)
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
