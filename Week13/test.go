a0c65fe7ffff2d66a5ffdc09225faa89e119605b
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-11-20 11:52:11 +0900

    v6

--- Week13/test.go
+++ Week13/test.go
@@ -1,12 +1,30 @@
+// average2 calculates the average of several numbers.
 package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
-func main() {
+func average(numbers ...float64) float64 {
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
 
-	fmt.Println(os.Args[1:])
-	fmt.Println(os.Args[2:])
+func main() {
+	arguments := os.Args[1:]
+	var numbers []float64
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+	fmt.Printf("Average: %0.2f\n", average(numbers...))
 }
