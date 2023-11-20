1de106bec7a967f89019b650204917388644f629
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-11-20 11:57:57 +0900

    v7

--- Week13/book.go
+++ Week13/book.go
@@ -1,46 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
+
+	"github.com/headfirstgo/datafile"
 )
 
-func GetFloats(fileName string) ([]float64, error) {
-	var numbers []float64
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		number, err := strconv.ParseFloat(scanner.Text(), 64)
-		if err != nil {
-			return nil, err
-		}
-		numbers = append(numbers, number)
-	}
-	err = file.Close()
-	if err != nil {
-		return nil, err
-	}
-	if scanner.Err() != nil {
-		return nil, scanner.Err()
-	}
-	return numbers, nil
-}
-
 func main() {
-	numbers, err := GetFloats("data.txt")
+	lines, err := datafile.GetStrings("votes.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var sum float64 = 0
-	for _, number := range numbers {
-		sum += number
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
 	}
-	sampleCount := float64(len(numbers))
-	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	for name, count := range counts {
+		fmt.Printf("Votes for %s: %d\n", name, count)
+	}
 }
